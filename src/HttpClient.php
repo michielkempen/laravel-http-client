78ca00947813cc869347dcaaf8f6d35ad8fe045a
Michiel Kempen <kempenmichiel@gmail.com>
2019-09-24 15:32:16 +0200

    forward payload of failing requests

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -4,6 +4,8 @@ namespace MichielKempen\LaravelHttpClient;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -15,16 +17,16 @@ class HttpClient
      */
     protected $client;
 
-	/**
-	 * @var bool
-	 */
-	protected $handleExceptions;
+    /**
+     * @var bool
+     */
+    protected $handleExceptions;
 
-	/**
-	 * @param string $host
-	 * @param array $headers
-	 * @param bool $handleExceptions
-	 */
+    /**
+     * @param string $host
+     * @param array $headers
+     * @param bool $handleExceptions
+     */
     public function __construct(string $host, array $headers = [], bool $handleExceptions = true)
     {
         $this->client = new Client([
@@ -32,147 +34,151 @@ class HttpClient
             'headers' => $headers
         ]);
 
-		$this->handleExceptions = $handleExceptions;
-	}
-
-	/**
-	 * @param Request $request
-	 * @return HttpResponse
-	 * @throws HttpException
-	 */
-    public function request(Request $request): HttpResponse
-    {
-    	switch ($request->method()) {
-			case 'GET':
-				return $this->get($request->path(), $request->all());
-			case 'POST':
-				return $this->post($request->path(), $request->all());
-			case 'PUT':
-				return $this->put($request->path(), $request->all());
-			case 'PATCH':
-				return $this->patch($request->path(), $request->all());
-			case 'DELETE':
-				return $this->delete($request->path(), $request->all());
-			default:
-				throw new HttpException("Unknown HTTP method '{$request->method()}'.", 500);
-		}
+        $this->handleExceptions = $handleExceptions;
     }
 
-	/**
-	 * @param string $url
-	 * @param array $parameters
-	 * @return HttpResponse
-	 * @throws HttpException
-	 */
+    /**
+     * @param Request $request
+     * @return HttpResponse
+     * @throws HttpException
+     */
+    public function request(Request $request): HttpResponse
+    {
+        switch ($request->method()) {
+            case 'GET':
+                return $this->get($request->path(), $request->all());
+            case 'POST':
+                return $this->post($request->path(), $request->all());
+            case 'PUT':
+                return $this->put($request->path(), $request->all());
+            case 'PATCH':
+                return $this->patch($request->path(), $request->all());
+            case 'DELETE':
+                return $this->delete($request->path(), $request->all());
+            default:
+                throw new HttpException("Unknown HTTP method '{$request->method()}'.", 500);
+        }
+    }
+
+    /**
+     * @param string $url
+     * @param array $parameters
+     * @return HttpResponse
+     * @throws HttpException
+     */
     public function get(string $url, array $parameters = []): HttpResponse
     {
         try {
-			$response = $this->client->get($url, [
-				'query' => $parameters,
-			]);
+            $response = $this->client->get($url, [
+                'query' => $parameters,
+            ]);
         } catch (RequestException $exception) {
-			$response = $exception->getResponse();
-            $this->handleException($response);
+            $this->handleException($exception);
         }
 
-		return new HttpResponse($response);
+        return new HttpResponse($response);
     }
 
-	/**
-	 * @param string $url
-	 * @param array $payload
-	 * @return HttpResponse
-	 * @throws HttpException
-	 */
-	public function post(string $url, array $payload = []): HttpResponse
-	{
-		try {
-			$response = $this->client->post($url, [
-				'json' => $payload,
-			]);
-		} catch (RequestException $exception) {
-			$response = $exception->getResponse();
-			$this->handleException($response);
-		}
+    /**
+     * @param string $url
+     * @param array $payload
+     * @return HttpResponse
+     * @throws HttpException
+     */
+    public function post(string $url, array $payload = []): HttpResponse
+    {
+        try {
+            $response = $this->client->post($url, [
+                'json' => $payload,
+            ]);
+        } catch (RequestException $exception) {
+            $this->handleException($exception);
+        }
 
-		return new HttpResponse($response);
-	}
+        return new HttpResponse($response);
+    }
 
-	/**
-	 * @param string $url
-	 * @param array $payload
-	 * @return HttpResponse
-	 * @throws HttpException
-	 */
+    /**
+     * @param string $url
+     * @param array $payload
+     * @return HttpResponse
+     * @throws HttpException
+     */
     public function put(string $url, array $payload = []): HttpResponse
     {
         try {
             $response = $this->client->put($url, [
-            	'json' => $payload,
+                'json' => $payload,
             ]);
         } catch (RequestException $exception) {
-			$response = $exception->getResponse();
-            $this->handleException($response);
+            $this->handleException($exception);
         }
 
         return new HttpResponse($response);
     }
 
-	/**
-	 * @param string $url
-	 * @param array $payload
-	 * @return HttpResponse
-	 * @throws HttpException
-	 */
+    /**
+     * @param string $url
+     * @param array $payload
+     * @return HttpResponse
+     * @throws HttpException
+     */
     public function patch(string $url, array $payload = []): HttpResponse
     {
         try {
             $response = $this->client->patch($url, [
-            	'json' => $payload,
+                'json' => $payload,
             ]);
         } catch (RequestException $exception) {
-			$response = $exception->getResponse();
-            $this->handleException($response);
+            $this->handleException($exception);
         }
 
         return new HttpResponse($response);
     }
 
-	/**
-	 * @param string $url
-	 * @param array $parameters
-	 * @return HttpResponse
-	 * @throws HttpException
-	 */
-	public function delete(string $url, array $parameters = []): HttpResponse
-	{
-		try {
-			$response = $this->client->delete($url, [
-				'query' => $parameters,
-			]);
-		} catch (RequestException $exception) {
-			$response = $exception->getResponse();
-			$this->handleException($response);
-		}
-
-		return new HttpResponse($response);
-	}
-
-	/**
-	 * @param Response $response
-	 * @throws HttpException
-	 */
-    protected function handleException(Response $response): void
+    /**
+     * @param string $url
+     * @param array $parameters
+     * @return HttpResponse
+     * @throws HttpException
+     */
+    public function delete(string $url, array $parameters = []): HttpResponse
     {
-    	if($this->handleExceptions) {
-			$body = (string) $response->getBody();
-			$code = (int) $response->getStatusCode();
+        try {
+            $response = $this->client->delete($url, [
+                'query' => $parameters,
+            ]);
+        } catch (RequestException $exception) {
+            $this->handleException($exception);
+        }
 
-			$content = json_decode($body);
+        return new HttpResponse($response);
+    }
 
-			$message = isset($content->message) ? $content->message : 'API request not processed.';
+    /**
+     * @param Response $response
+     * @throws HttpException
+     */
+    protected function handleException(RequestException $exception): void
+    {
+        if(! $this->handleExceptions) {
+            return;
+        }
 
-			throw new HttpException($message, $code);
-		}
+        if($exception instanceof ConnectException) {
+            throw new HttpException($exception->getHandlerContext()['error'], 500);
+        }
+
+        if($exception instanceof ClientException) {
+            $response = $exception->getResponse();
+
+            $status = $response->getStatusCode();
+            $payload = json_decode($response->getBody()->getContents(), true) ?? [];
+            $message = $payload['message'] ?? 'API request not processed.';
+
+            throw new HttpException($message, $status, $payload);
+        }
+
+        throw $exception;
     }
 }
--- src/HttpException.php
+++ src/HttpException.php
@@ -6,27 +6,43 @@ use Illuminate\Http\JsonResponse;
 
 class HttpException extends \Exception
 {
-	/**
-	 * Exception constructor.
-	 *
-	 * @param string $message
-	 * @param int $statusCode
-	 */
-	public function __construct(string $message, int $statusCode)
-	{
-		parent::__construct($message, $statusCode);
-	}
+    /**
+     * @var null|array
+     */
+    private $payload;
 
-	/**
-	 * Render the exception into an HTTP response.
-	 *
-	 * @return JsonResponse
-	 */
-	public function render()
-	{
-		return new JsonResponse([
-			'message' => $this->getMessage(),
-			'status' => $this->getCode(),
-		], $this->getCode());
-	}
+    /**
+     * Exception constructor.
+     *
+     * @param string $message
+     * @param int $statusCode
+     * @param array $payload
+     */
+    public function __construct(string $message, int $statusCode, ?array $payload = null)
+    {
+        parent::__construct($message, $statusCode);
+
+        $this->payload = $payload;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getPayload(): ?array
+    {
+        return $this->payload;
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @return JsonResponse
+     */
+    public function render()
+    {
+        return new JsonResponse(
+            $this->payload ?? ['message' => $this->getMessage(), 'status' => $this->getCode()],
+            $this->getCode()
+        );
+    }
 }
