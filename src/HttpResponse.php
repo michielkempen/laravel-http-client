e1f73dd08c0577a354ec9378abefc24008c9e5b3
Michiel Kempen <kempenmichiel@gmail.com>
2021-02-19 18:05:18 +0100

    wip

--- src/HttpResponse.php
+++ src/HttpResponse.php
@@ -3,31 +3,102 @@
 namespace MichielKempen\LaravelHttpClient;
 
 use Illuminate\Contracts\Support\Responsable;
-use Illuminate\Http\JsonResponse;
 use Psr\Http\Message\ResponseInterface as Response;
-use stdClass;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class HttpResponse implements Responsable
 {
-	private Response $response;
+    /**
+     * The underlying PSR response.
+     */
+	protected Response $response;
 
+    /**
+     * Create a new HTTP response.
+     */
 	public function __construct(Response $response)
 	{
 		$this->response = $response;
 	}
 
-	public function status(): int
+    /**
+     * Get the status code of the response.
+     */
+	public function getStatusCode(): int
 	{
-		return $this->response->getStatusCode();
+		return (int) $this->response->getStatusCode();
 	}
 
-	public function toObject(): stdClass
+    /**
+     * Determine if the request was successful.
+     */
+    public function isSuccessful(): bool
+    {
+        return $this->getStatusCode() >= 200 && $this->getStatusCode() < 300;
+    }
+
+    /**
+     * Determine if a client error occurred.
+     */
+    public function clientErrorOccurred(): bool
+    {
+        return $this->getStatusCode() >= 400 && $this->getStatusCode() < 500;
+    }
+
+    /**
+     * Determine if a server error occurred.
+     */
+    public function serverErrorOccurred(): bool
+    {
+        return $this->getStatusCode() >= 500;
+    }
+
+    /**
+     * Determine if a client or server error occurred.
+     */
+    public function errorOccurred(): int
+    {
+        return $this->serverErrorOccurred() || $this->clientErrorOccurred();
+    }
+
+    /**
+     * Get the content type of the response.
+     */
+    public function getContentType(): string
+    {
+        return $this->response->getHeaderLine('Content-Type');
+    }
+
+    /**
+     * Determine if the content type of the response is JSON.
+     */
+    public function containsJson(): bool
+    {
+        return $this->getContentType() === 'application/json';
+    }
+
+    /**
+     * Get the body of the response.
+     */
+    public function getBody(): string
+    {
+        return (string) $this->response->getBody();
+    }
+
+    /**
+     * Get the JSON decoded body of the response as an object.
+     */
+	public function toObject(): object
 	{
 		$body = (string) $this->response->getBody();
 
-		return json_decode($body) ?? new stdClass;
+		return json_decode($body) ?? (object) [];
 	}
 
+    /**
+     * Get the JSON decoded body of the response as an array.
+     */
 	public function toArray(): array
 	{
 		$body = (string) $this->response->getBody();
@@ -35,13 +106,19 @@ class HttpResponse implements Responsable
 		return json_decode($body, true) ?? [];
 	}
 
-	public function toString(): string
+    /**
+     * Get the Symfony representation of the response.
+     */
+	public function toResponse($request): SymfonyResponse
 	{
-		return (string) $this->response->getBody();
+        return (new HttpFoundationFactory)->createResponse($this->response);
 	}
 
-	public function toResponse($request)
-	{
-		return new JsonResponse($this->toArray(), $this->status());
-	}
+    /**
+     * Get the body of the response.
+     */
+    public function __toString(): string
+    {
+        return $this->getBody();
+    }
 }
